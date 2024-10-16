dcf118ea6485d956fd6327eb2ffb380a8853026c
diedgo05 <233358@ids.upchiapas.edu.mx>
2024-10-16 15:48:54 -0600

    API funcionando

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_jwt_extended import JWTManager
+from config import config
+from src.routes.userRoutes import usuario_blueprint
+from src.models.user import db
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(config['development'])
+    db.init_app(app)
+    jwt = JWTManager(app)
+    app.register_blueprint(usuario_blueprint)
+    return app
+
+if __name__ == '__main__':
+    app = create_app()
+    app.run(debug=True)
