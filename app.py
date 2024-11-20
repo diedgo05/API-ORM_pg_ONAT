397eb1e42825a79783fc884a536630d06afcf5e8
diedgo05 <233358@ids.upchiapas.edu.mx>
2024-11-20 13:44:21 -0600

    Terminado

--- app.py
+++ app.py
@@ -23,11 +23,12 @@ def create_app():
     app.register_blueprint(drive_bp)
 
     return app
+    
+app = create_app()
+with app.app_context():
+    db.create_all()
 
 if __name__ == '__main__':
-    app = create_app()
-    with app.app_context():
-        db.create_all()
+
     app.run(debug=True)
 
-    
